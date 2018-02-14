cea68a4d8b26ff1dc5ef169de059f5afb03bda04
Pratik Chaudhari <chaudharipratikg@gmail.com>
2018-02-14 09:53:39 +0530

    Added comment.rb

---
+++ comment/comment.rb
@@ -0,0 +1,5 @@
+# This is how a comment is written in ruby
+
+puts "If you see this line that means the code that created this wasn't commented"
+
+# puts "This line won't be printed because it's code has been commented. If you see this line that means someone has uncommented it's code"
