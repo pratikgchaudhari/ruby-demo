fb52e7c682eac7f1f45e97767c1e121860399369
Pratik Chaudhari <chaudharipratikg@gmail.com>
2018-02-15 10:52:02 +0530

    Added number.rb

---
+++ number/number.rb
@@ -0,0 +1,15 @@
+print "Enter a integer: "
+integer = gets.chomp.to_i
+puts "#{integer} * 10 = #{integer * 10}"
+
+print "\nEnter another integer: "
+integer = gets.chomp.to_i
+puts "#{integer} / 10 = #{integer/10}"
+
+print "\nEnter a floating point number: "
+float = gets.chomp.to_f
+puts "#{float} * 10 = #{float * 10}"
+
+print "\nEnter another floating point number: "
+float = gets.chomp.to_f
+puts "#{float} / 10 = #{float / 10}"
