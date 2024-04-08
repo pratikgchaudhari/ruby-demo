e3b397f5ca08a8af0bb82b8222f6343f8ad2113b
Pratik Chaudhari <chaudharipratikg@gmail.com>
2024-04-08 23:30:20 +0530

    Add example of multiline comment in Ruby
    This commit introduces a new file (multiline_comments.rb) that contains an example of how to use multiline comments in Ruby. The commented area also includes an explanation about its features, discussing how it avoids the need to use '#'-prefix for each line.

---
+++ multiline_comments/multiline_comments.rb
@@ -0,0 +1,5 @@
+=begin
+  This is example of a multi-line comment in Ruby.
+  It doesn't require one to prefix each line with the '#' character.
+  One can add as many lines one wishes using this type of comment
+=end
