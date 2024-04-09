aa1a94134141be8b8af8495995ce9953eb058321
Pratik Chaudhari <chaudharipratikg@gmail.com>
2024-04-09 08:03:07 +0530

    Disable Rubocop inspections for comments
    The Rubocop inspections for block comments and line length were temporarily disabled to accommodate certain comments. These inspections were reenabled afterwards, ensuring that the overall style guidelines of Rubocop are still applied to the project.

--- comment/comment.rb
+++ comment/comment.rb
@@ -2,4 +2,8 @@
 
 puts "If you see this line that means the code that created this wasn't commented"
 
+# rubocop: disable Layout/LineLength
+
 # puts "This line won't be printed because it's code has been commented. If you see this line that means someone has uncommented it's code"
+
+# rubocop: enable Layout/LineLength
--- multiline_comments/multiline_comments.rb
+++ multiline_comments/multiline_comments.rb
@@ -1,5 +1,9 @@
+# rubocop: disable Style/BlockComments
+
 =begin
   This is example of a multi-line comment in Ruby.
   It doesn't require one to prefix each line with the '#' character.
   One can add as many lines one wishes using this type of comment
 =end
+
+# rubocop: enable Style/BlockComments
